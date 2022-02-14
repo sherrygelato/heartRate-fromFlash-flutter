0f49d3fb874063cb4ec332cafbaa0d7b62e34e5c
sherrygelato <sherrygelato@kakao.com>
2022-02-14 17:17:09 +0900

    clear

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,134 +1,53 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 import 'chart.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   HomePageView createState() {
     return HomePageView();
   }
 }
 
-class HomePageView extends State<HomePage> {
-  bool _toggled = false;
-  bool _processing = false;
-  final List<SensorValue> _data = [];
+class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
+  bool _toggled = false; // toggle button value
+  List<SensorValue> _data = <SensorValue>[]; // array to store the values
   CameraController? _controller;
-  final double _alpha = 0.3;
-  final int _bpm = 0;
+  double _alpha = 0.3; // factor for the mean value
+  AnimationController? _animationController;
+  double _iconScale = 1;
+  int _bpm = 0; // beats per minute
+  int _fs = 30; // sampling frequency (fps)
+  int _windowLen = 30 * 6; // window length to display - 6 seconds
+  CameraImage? _image; // store the last camera image
+  double? _avg; // store the average value during calculation
+  DateTime? _now; // store the now Datetime
+  Timer? _timer; // timer for image processing
 
-  _toggle() {
-    _initController().then((onValue) {
-      Wakelock.enable();
-      setState(() {
-        _toggled = true;
-        _processing = false;
-      });
-      _updateBPM();
-    });
-  }
-
-  _untoggle() {
-    _disposeController();
-    Wakelock.disable();
-    setState(() {
-      _toggled = false;
-      _processing = false;
-    });
-  }
-
-  Future<void> _initController() async {
-    try {
-      List _cameras = await availableCameras();
-      _controller = CameraController(_cameras.first, ResolutionPreset.low);
-      await _controller?.initialize();
-      Future.delayed(const Duration(milliseconds: 500)).then((onValue) {
-        _controller?.setFlashMode(FlashMode.auto);
-      });
-      _controller?.startImageStream((CameraImage image) {
-        if (!_processing) {
-          setState(() {
-            _processing = true;
-          });
-          _scanImage(image);
-        }
-      });
-    } catch (Exception) {
-      print(Exception);
-    }
-  }
-
-  _updateBPM() async {
-    List<SensorValue> _values;
-    double _avg;
-    int _n;
-    double _m;
-    double _threshold;
-    double _bpm;
-    int _counter;
-    int _previous;
-    while (_toggled) {
-      _values = List.from(_data);
-      _avg = 0;
-      _n = _values.length;
-      _m = 0;
-      for (var value in _values) {
-        _avg += value.value / _n;
-        if (value.value > _m) _m = value.value;
-      }
-      _threshold = (_m + _avg) / 2;
-      _bpm = 0;
-      _counter = 0;
-      _previous = 0;
-      for (int i = 1; i < _n; i++) {
-        if (_values[i - 1].value < _threshold &&
-            _values[i].value > _threshold) {
-          if (_previous != 0) {
-            _counter++;
-            _bpm +=
-                60000 / (_values[i].time.millisecondsSinceEpoch - _previous);
-          }
-          _previous = _values[i].time.millisecondsSinceEpoch;
-        }
-      }
-      if (_counter > 0) {
-        _bpm = _bpm / _counter;
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    _animationController!
+      ..addListener(() {
         setState(() {
-          _bpm = (1 - _alpha) * _bpm + _alpha * _bpm;
+          _iconScale = 1.0 + _animationController!.value * 0.4;
         });
-      }
-      await Future.delayed(Duration(milliseconds: (1000 * 50 / 30).round()));
-    }
-  }
-
-  _scanImage(CameraImage image) {
-    double _avg =
-        image.planes.first.bytes.reduce((value, element) => value + element) /
-            image.planes.first.bytes.length;
-    if (_data.length >= 50) {
-      _data.removeAt(0);
-    }
-    setState(() {
-      _data.add(SensorValue(DateTime.now(), _avg));
-    });
-    Future.delayed(const Duration(milliseconds: 1000 ~/ 30)).then((onValue) {
-      setState(() {
-        _processing = false;
       });
-    });
-  }
-
-  _disposeController() {
-    _controller!.dispose();
-    _controller;
   }
 
   @override
   void dispose() {
+    _timer?.cancel();
+    _toggled = false;
     _disposeController();
+    Wakelock.disable();
+    _animationController?.stop();
+    _animationController?.dispose();
     super.dispose();
   }
 
@@ -140,49 +59,99 @@ class HomePageView extends State<HomePage> {
         child: Column(
           children: <Widget>[
             Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Center(
-                      child: _controller == null
-                          ? Container()
-                          : CameraPreview(_controller!),
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        (_bpm > 30 && _bpm < 150
-                            ? _bpm.round().toString()
-                            : "--"),
-                        style: const TextStyle(
-                            fontSize: 32, fontWeight: FontWeight.bold),
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(18),
+                          ),
+                          child: Stack(
+                            fit: StackFit.expand,
+                            alignment: Alignment.center,
+                            children: <Widget>[
+                              _controller != null && _toggled
+                                  ? AspectRatio(
+                                      aspectRatio:
+                                          _controller!.value.aspectRatio,
+                                      child: CameraPreview(_controller!),
+                                    )
+                                  : Container(
+                                      padding: EdgeInsets.all(12),
+                                      alignment: Alignment.center,
+                                      color: Colors.grey,
+                                    ),
+                              Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(4),
+                                child: Text(
+                                  _toggled
+                                      ? "Cover both the camera and the flash with your finger"
+                                      : "Camera feed will display here",
+                                  style: TextStyle(
+                                      backgroundColor: _toggled
+                                          ? Colors.white
+                                          : Colors.transparent),
+                                  textAlign: TextAlign.center,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
+                    Expanded(
+                      flex: 1,
+                      child: Center(
+                          child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Estimated BPM",
+                            style: TextStyle(fontSize: 18, color: Colors.grey),
+                          ),
+                          Text(
+                            (_bpm > 30 && _bpm < 150 ? _bpm.toString() : "--"),
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )),
+                    ),
+                  ],
+                )),
             Expanded(
+              flex: 1,
               child: Center(
-                child: IconButton(
-                  icon: Icon(_toggled ? Icons.favorite : Icons.favorite_border),
-                  color: Colors.red,
-                  iconSize: 128,
-                  onPressed: () {
-                    if (_toggled) {
-                      _untoggle();
-                    } else {
-                      _toggle();
-                    }
-                  },
+                child: Transform.scale(
+                  scale: _iconScale,
+                  child: IconButton(
+                    icon:
+                        Icon(_toggled ? Icons.favorite : Icons.favorite_border),
+                    color: Colors.red,
+                    iconSize: 128,
+                    onPressed: () {
+                      if (_toggled) {
+                        _untoggle();
+                      } else {
+                        _toggle();
+                      }
+                    },
+                  ),
                 ),
               ),
             ),
             Expanded(
+              flex: 1,
               child: Container(
-                margin: const EdgeInsets.all(12),
-                decoration: const BoxDecoration(
+                margin: EdgeInsets.all(12),
+                decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(
                       Radius.circular(18),
                     ),
@@ -194,5 +163,137 @@ class HomePageView extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  void _clearData() {
+    // create array of 128 ~= 255/2
+    _data.clear();
+    int now = DateTime.now().millisecondsSinceEpoch;
+    for (int i = 0; i < _windowLen; i++)
+      _data.insert(
+          0,
+          SensorValue(
+              DateTime.fromMillisecondsSinceEpoch(now - i * 1000 ~/ _fs), 128));
+  }
+
+  void _toggle() {
+    _clearData();
+    _initController().then((onValue) {
+      Wakelock.enable();
+      _animationController?.repeat(reverse: true);
+      setState(() {
+        _toggled = true;
+      });
+      // after is toggled
+      _initTimer();
+      _updateBPM();
+    });
+  }
+
+  void _untoggle() {
+    _disposeController();
+    Wakelock.disable();
+    _animationController?.stop();
+    _animationController?.value = 0.0;
+    setState(() {
+      _toggled = false;
+    });
+  }
+
+  void _disposeController() {
+    _controller?.dispose();
+    _controller = null;
+  }
+
+  Future<void> _initController() async {
+    try {
+      List _cameras = await availableCameras();
+      _controller = CameraController(_cameras.first, ResolutionPreset.low);
+      await _controller!.initialize();
+      Future.delayed(Duration(milliseconds: 100)).then((onValue) {
+        _controller!.setFlashMode(FlashMode.torch);
+      });
+      _controller!.startImageStream((CameraImage image) {
+        _image = image;
+      });
+    } catch (Exception) {
+      print(Exception);
+    }
+  }
+
+  void _initTimer() {
+    _timer = Timer.periodic(Duration(milliseconds: 1000 ~/ _fs), (timer) {
+      if (_toggled) {
+        if (_image != null) _scanImage(_image!);
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void _scanImage(CameraImage image) {
+    _now = DateTime.now();
+    _avg =
+        image.planes.first.bytes.reduce((value, element) => value + element) /
+            image.planes.first.bytes.length;
+    if (_data.length >= _windowLen) {
+      _data.removeAt(0);
+    }
+    setState(() {
+      _data.add(SensorValue(_now!, 255 - _avg!));
+    });
+  }
+
+  void _updateBPM() async {
+    // Bear in mind that the method used to calculate the BPM is very rudimentar
+    // feel free to improve it :)
+
+    // Since this function doesn't need to be so "exact" regarding the time it executes,
+    // I only used the a Future.delay to repeat it from time to time.
+    // Ofc you can also use a Timer object to time the callback of this function
+    List<SensorValue> _values;
+    double _avg;
+    int _n;
+    double _m;
+    double _threshold;
+    double _bpm;
+    int _counter;
+    int _previous;
+    while (_toggled) {
+      _values = List.from(_data); // create a copy of the current data array
+      _avg = 0;
+      _n = _values.length;
+      _m = 0;
+      _values.forEach((SensorValue value) {
+        _avg += value.value / _n;
+        if (value.value > _m) _m = value.value;
+      });
+      _threshold = (_m + _avg) / 2;
+      _bpm = 0;
+      _counter = 0;
+      _previous = 0;
+      for (int i = 1; i < _n; i++) {
+        if (_values[i - 1].value < _threshold &&
+            _values[i].value > _threshold) {
+          if (_previous != 0) {
+            _counter++;
+            _bpm += 60 *
+                1000 /
+                (_values[i].time.millisecondsSinceEpoch - _previous);
+          }
+          _previous = _values[i].time.millisecondsSinceEpoch;
+        }
+      }
+      if (_counter > 0) {
+        _bpm = _bpm / _counter;
+        print(_bpm);
+        setState(() {
+          this._bpm = ((1 - _alpha) * this._bpm + _alpha * _bpm).toInt();
+        });
+      }
+      await Future.delayed(Duration(
+          milliseconds:
+              1000 * _windowLen ~/ _fs)); // wait for a new set of _data values
+    }
   }
 }
